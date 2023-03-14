a9bc30c9395b4624ee62a527bfb663baad7e18ce
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2023-03-14 10:28:56 +0100

    implementation of next page token

--- automatize_like_comment_add.py
+++ automatize_like_comment_add.py
@@ -20,7 +20,6 @@ DEVELOPER_KEY = open("secret_key.txt").readlines()[0]
 
 
 
-
 def get_all_ids_in_playlist(youtube_api, playlist_id):
   ids = []
   nextPageToken = "something"
@@ -44,39 +43,60 @@ def get_all_ids_in_playlist(youtube_api, playlist_id):
   
   return list(set(ids)) # just to be sure 
 
-def youtube_search(youtube_api,list_id_in_playlist,options):
+
+def youtube_search(youtube_api,list_id_in_playlist,options, order="date", limit_pages = 10):
 
   # Call the search.list method to retrieve results matching the specified
   # query term.
-  search_response = youtube_api.search().list(
-    q=options.q,
-    part='id,snippet',
-    order = "date",
-    maxResults=options.max_results
-  ).execute()
-
-  videos = []
+  nextPageToken = "something"
+  i = 0
+  
+  # videos = []
   new_ids = []
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get('items', []):
-    if search_result['id']['kind'] == 'youtube#video':
-      videos.append({"id":search_result['id']['videoId'], "title": search_result['snippet']['title']})
-      new_ids.append(search_result['id']['videoId'])
+  
+  while (nextPageToken):
+    
+    # print(nextPageToken)
+    search_response = youtube_api.search().list(
+      q=options.q,
+      part='id,snippet',
+      order = order,
+      maxResults=50,
+      type="video",
+      pageToken = None if i==0 else nextPageToken
+    ).execute()
+
+ 
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+      if search_result['id']['kind'] == 'youtube#video':
+        # videos.append({"id":search_result['id']['videoId'], "title": search_result['snippet']['title']})
+        new_ids.append(search_result['id']['videoId'])
+
+    i+=1
+    
+    if i==limit_pages:
+      break
+    if "nextPageToken" in search_response.keys():
+      nextPageToken = search_response["nextPageToken"]
+    else:
+      nextPageToken = None
+  
   print(len(new_ids))
 
   # FILTER NEW IDS 
   new_ids = list(set(new_ids).difference(set(list_id_in_playlist)))
   # print(len(new_ids))
-  # print(new_ids)
-  return sorted(new_ids)
+  print(new_ids)
+  return new_ids# sorted(new_ids)
 
-def like_comment_add_video_ids(youtube_api,list_ids, comment_template, playlistId_to_add_template, index_start):
+def like_comment_add_video_ids(youtube_api,list_ids, comment_templates, playlistId_to_add_template, index_start):
   report_links = []
-  for ids in tqdm.tqdm(list_ids):
+  for idx,ids in tqdm.tqdm(enumerate(list_ids), total=len(list_ids), desc="Automatizing actions"):
     time.sleep(10)
     video_url = "https://www.youtube.com/watch?v=%s" %ids
-    print("video link %s" % video_url)
+    print("video link %s" % video_url)   
     
     try:
       # comment
@@ -88,13 +108,13 @@ def like_comment_add_video_ids(youtube_api,list_ids, comment_template, playlistI
               "videoId": ids,
               "topLevelComment": {
                 "snippet": {
-                  "textOriginal": comment_template
+                  "textOriginal": comment_templates[idx%len(comment_templates)]
                 }
               }
             }
           }
           ).execute()
-      time.sleep(5)
+      time.sleep(10)
       # like
       youtube_api.videos().rate(
           id = ids,
@@ -115,7 +135,7 @@ def like_comment_add_video_ids(youtube_api,list_ids, comment_template, playlistI
             }
           }
       ).execute()
-      time.sleep(5)
+      time.sleep(10)
       report_links.append(video_url)
       index_start +=1
     except HttpError as e:
@@ -130,8 +150,8 @@ def like_comment_add_video_ids(youtube_api,list_ids, comment_template, playlistI
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--q', help='Search term', default='digimon amv|digimon asmv')
-  parser.add_argument('--max-results', help='Max results', default=10)
+  parser.add_argument('--q', help='Search term', default='digimon amv')
+  parser.add_argument('--max-results', help='Max results', default=50)
   args = parser.parse_args()
   
   token_secrets = json.load(open("token.json"))
@@ -152,17 +172,18 @@ if __name__ == '__main__':
   try:
     list_id_in_playlist = get_all_ids_in_playlist(youtube_api=youtube_api, playlist_id=playlist_id)
     novel_id_to_comment_and_add = youtube_search(youtube_api,list_id_in_playlist,args)
-    comment_template = "".join(open("comment_template.txt", encoding='utf-8').readlines())
-    
+    comment_templates = list(open("template_comments/templates.txt", encoding='utf-8').readlines())
+    comment_templates = [x.rstrip('\n') for x in comment_templates]
     links_report = like_comment_add_video_ids(youtube_api=youtube_api,list_ids=novel_id_to_comment_and_add, 
-                   comment_template=comment_template, playlistId_to_add_template = playlist_id, index_start = len(list_id_in_playlist))
+                   comment_templates=comment_templates, playlistId_to_add_template = playlist_id, index_start = len(list_id_in_playlist))
     # plot like and commented video
-    
-    td_string = datetime.now().strftime("%d_%m_%Y")
-    
-    with open("report_%s.txt" % td_string, "w") as f:
-      for l in links_report:
-        f.write(l)
-        
   except HttpError as e:
-    print ('An HTTP error %d occurred:\n%s' % (e.resp.status, e.reason))    
+    print ('An HTTP error %d occurred:\n%s' % (e.resp.status, e.reason))
+    exit()    
+  
+  td_string = datetime.now().strftime("%d_%m_%Y")
+  
+  with open("report_%s.txt" % td_string, "w") as f:
+    for l in links_report:
+      f.write(l)
+        
